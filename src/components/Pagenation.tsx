8a20d267638daa14a36896364fd79ddf389d7e76
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-03-07 10:32:48 +0900

    fix previous btn disabled

--- src/components/Pagenation.tsx
+++ src/components/Pagenation.tsx
@@ -9,14 +9,15 @@ const Pagenation: FC<Props> = ({ index, setIndex }) => {
   return (
     <nav aria-label="Page navigation example">
       <ul className="inline-flex -space-x-px">
-        {<li>
+        <li>
           <button 
+            disabled={index <= 0}
             className="px-3 py-2 ml-0 leading-tight border rounded-l-lg bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-700 hover:text-white"
             onClick={() => setIndex(index - 1)}
             >
               Previous
           </button>
-        </li>}
+        </li>
         <li>
           <button 
             className="px-3 py-2 ml-0 leading-tight border rounded-r-lg bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-700 hover:text-white"
