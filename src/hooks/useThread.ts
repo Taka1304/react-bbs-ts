3b185d40e0548c7d854ff73dc8f7571e6fa90e8e
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-03-08 10:22:54 +0900

    Add fetch POST create new thread

--- src/hooks/useThread.ts
+++ src/hooks/useThread.ts
@@ -1,10 +1,12 @@
 import useSWR from 'swr'
-import { Thread } from 'src/types'
+import { Thread, ThreadCreateResponse } from 'src/types'
+import { useState } from 'react'
 
 const fetcher = (url: string) => fetch(url).then(res => res.json())
+const threadUrl = "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads"
 
 export const useThreadList = (offset=0) => {
-  const { data, error, isLoading } = useSWR<Thread[], Error>(`https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads?offset=${offset}`, fetcher)
+  const { data, error, isLoading } = useSWR<Thread[], Error>(`${threadUrl}?offset=${offset}`, fetcher)
 
   return {
     threadList: data,
@@ -13,10 +15,36 @@ export const useThreadList = (offset=0) => {
   }
 }
 
-export const useMakeThread = () => {
 
-}
+export const useCreateThread = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [data, setData] = useState<ThreadCreateResponse | undefined>()
 
-export const usePostList = (threadId :string) => {
-
+  
+  const fetchPost = async(threadTitle: string) => {
+    const sendData = { title: threadTitle }
+    try {
+      setIsLoading(true)
+      const res = await fetch(threadUrl, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(sendData)
+      })
+      if (res.ok) {
+        setData(await res.json())
+        console.log(res.json())
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  } 
+  return {
+    data,
+    fetchPost,
+    isLoading
+  }
 }
