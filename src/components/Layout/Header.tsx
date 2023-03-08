c698ecdc87c0c93c0942be85ffe39ba0b1961f8f
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-03-08 10:25:08 +0900

    fix header  navigator

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,14 +1,16 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Header: FC = () => {
+  const router = useLocation()
   return (
     <header className='flex flex-row items-center justify-between bg-[#282c34] text-white h-16 px-4'>
       <span className='text-2xl font-bold'>
         掲示板
       </span>
       <nav className='flex items-center underline'>
-        <a href="/thread/new" className="m-2 md:text-lg">新規スレッド</a>
-        <a href="/" className="m-2 md:text-lg">スレッド一覧</a>
+        {router.pathname !== "/thread/new" && <a href="/thread/new" className="m-2 md:text-lg">新規スレッド</a>}
+        {router.pathname !== "/" && <a href="/" className="m-2 md:text-lg">スレッド一覧</a>}
       </nav>
     </header>
   )
