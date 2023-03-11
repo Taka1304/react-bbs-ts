c17aee2edc9865776fc20fbc7bec7d854dbe98c6
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-03-11 16:05:50 +0900

    add select wrap

--- src/components/ThreadItem.tsx
+++ src/components/ThreadItem.tsx
@@ -2,14 +2,19 @@ import React, { FC, ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
+  wrap?: boolean
 }
 
-const ThreadItem: FC<Props> = ({ children }) => {
+const ThreadItem: FC<Props> = ({ children, wrap }) => {
   return (
     <div 
       className="items-center p-2"
       >
+      {wrap ? 
+      <p className='truncate whitespace-pre-wrap'>{children}</p>
+      :
       <p className='truncate'>{children}</p>
+      }
     </div>
   )
 }
